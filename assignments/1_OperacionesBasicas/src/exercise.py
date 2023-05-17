275fef7e038b51eb8f095df54d1e6d460b0906bf
Antonio de Jesus Gonzalez Castro <jegonzalez.corporativo@gmail.com>
2023-05-17 18:32:34 -0500

    Update exercise.py

--- assignments/1_OperacionesBasicas/src/exercise.py
+++ assignments/1_OperacionesBasicas/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    a = int(input("Ingrese el valor de a: "));
+    b = int(input("Ingrese el valor de b: "));
+    result_sum = a + b;
+    result_mult = a * b;
+    result_rest = a - b;
+ 
 
-
+print("La suma es: ", result_sum);
+print("La resta es: ", result_rest);
+print("La multiplicacion es: ", result_mult);
+  
 if __name__ == '__main__':
     main()
